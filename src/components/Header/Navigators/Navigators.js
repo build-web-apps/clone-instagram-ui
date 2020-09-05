13226264e20ef41800a7c4f4297033009bdeba62
Arunkumar Srisailapathi <thearunkumarsrisailapathi@gmail.com>
2020-09-05 15:39:17 +0530

    feat(search+nav): brings in search and navigation ui components

--- src/components/Header/Navigators/Navigators.js
+++ src/components/Header/Navigators/Navigators.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navigators.css';
 import { Toolbar, IconButton } from '@material-ui/core';
-import { Home, FavoriteBorderOutlined } from '@material-ui/icons';
+import { Home, FavoriteBorderOutlined, Add } from '@material-ui/icons';
 import { Profile } from './Profile';
 
 export const Navigators = ({ className }) => {
@@ -12,6 +12,9 @@ export const Navigators = ({ className }) => {
       </IconButton>
       <IconButton color="inherit" aria-label="menu">
         <FavoriteBorderOutlined />
+      </IconButton>
+      <IconButton color="inherit" aria-label="menu">
+        <Add />
       </IconButton>
       <IconButton color="inherit" aria-label="menu">
         <Profile src="https://instagram.fmaa3-1.fna.fbcdn.net/v/t51.2885-19/10724122_884280208257740_2025085888_a.jpg?_nc_ht=instagram.fmaa3-1.fna.fbcdn.net&_nc_ohc=jZjdH0PzYbUAX9BE1Pw&oh=9d55ce62392f2868ce86f1d6183892f5&oe=5F7C93A2" />
