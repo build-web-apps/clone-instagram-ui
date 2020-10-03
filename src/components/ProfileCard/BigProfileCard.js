2ec15a850e9fccb9e4e444ce623b21d18f2a409f
Arunkumar Srisailapathi <thearunkumarsrisailapathi@gmail.com>
2020-10-03 16:58:19 +0530

    feat(profile): adds profile view

---
+++ src/components/ProfileCard/BigProfileCard.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import './BigProfileCard.css';
+import { Avatar, Button } from '@material-ui/core';
+
+const COLOR_MAP = {
+  A: '#fb0c1c',
+  B: '#a44f5a',
+  C: '#a575bb',
+  D: '#b8749c',
+  E: '#7dfefc',
+  F: '#a0082e',
+  G: '#3bf231',
+  H: '#3d9b89',
+  I: '#77f5f2',
+  J: '#f3c312',
+  K: '#c79d27',
+  L: '#7563f8',
+  M: '#4356bd',
+  N: '#41f07e',
+  O: '#578044',
+  P: '#376126',
+  Q: '#d47064',
+  R: '#188cad',
+  S: '#e67869',
+  T: '#e216d5',
+  U: '#12b56',
+  V: '#b21e35',
+  W: '#3f8216',
+  X: '#9a2b51',
+  Y: '#4c6997',
+  Z: '#b96cda',
+};
+
+const DEFAULT_COLOR = 'grey';
+
+export const getBackgroundColor = (character) => {
+  return COLOR_MAP[character] || DEFAULT_COLOR;
+};
+
+export const BigProfileCard = ({
+  className = 'big-profile-card',
+  userName,
+  name,
+  description,
+  showFollowing,
+  isFollowing,
+  postsCount,
+  followersCount,
+  followingCount,
+  handleFollowClick,
+}) => {
+  const firstCharacter = userName[0].toUpperCase();
+  return (
+    <div className={className}>
+      <div className="avatar-area">
+        <Avatar
+          className="big-profile-avatar"
+          style={{ background: getBackgroundColor(firstCharacter) }}
+        >
+          {firstCharacter}
+        </Avatar>
+      </div>
+      <div className="content-area">
+        <div className="content-item">
+          <b className="username">{userName}</b>
+          {showFollowing && (
+            <Button
+              size="small"
+              variant={isFollowing ? 'outlined' : 'contained'}
+              color={isFollowing ? '' : 'primary'}
+              onClick={handleFollowClick}
+            >
+              {isFollowing ? 'Following' : 'Follow'}
+            </Button>
+          )}
+        </div>
+        <div className="content-item has-children-items">
+          <span className="content-secondary-item">
+            <b>{postsCount}</b> posts
+          </span>
+          <span className="content-secondary-item">
+            <b>{followersCount}</b> followers
+          </span>
+          <span className="content-secondary-item">
+            <b>{followingCount}</b> following
+          </span>
+        </div>
+        <div className="content-item">
+          <b>{name}</b>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getCall, postCall } from '../../utils/Network';
+import { Loader } from '../../components/Loader/Loader';
 
-export class Profile extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.username = props.match.params.username;
-  }
-  render() {
-    return <span>Welcome! {this.username}</span>;
-  }
-}
+import './Profile.css';
+import { BigProfileCard } from '../../components/ProfileCard/BigProfileCard';
+
+const GET_USER_API = '/users';
+const POST_FOLLOW_API = '/users/follow';
+const POST_UNFOLLOW_API = '/users/unfollow';
+
+export const Profile = ({ ...props }) => {
+  const username = props.match.params.username;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const getUserInformation = async (username) => {
+      try {
+        const response = await getCall(`${GET_USER_API}/${username}`);
+        const users = await response.json();
+        setLoading(false);
+        setError(false);
+        setUser({ ...users[0] });
+      } catch (err) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+
+    getUserInformation(username);
+  }, [username]);
+
+  const handleFollowClick = async (isFollowing = true) => {
+    let url = POST_FOLLOW_API;
+    if (isFollowing) {
+      url = POST_UNFOLLOW_API;
+    }
+    url += `/${username}`;
+    try {
+      const response = await postCall(
+        url,
+        JSON.stringify({
+          loggedInUser: user.userName,
+        })
+      );
+    } catch (err) {
+      console.log('Do something with the error');
+    }
+  };
+
+  return (
+    <div className="profile-view ins-body">
+      {loading && <Loader />}
+      {!loading && !error && (
+        <BigProfileCard
+          userName={username}
+          description={username}
+          name={user.name}
+          showFollowing={user.userName !== username}
+          isFollowing={false}
+          postsCount={1000}
+          followersCount={10000}
+          followingCount={10000}
+          handleFollowClick={handleFollowClick}
+        />
+      )}
+    </div>
+  );
+};
